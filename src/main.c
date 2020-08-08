de7bfe475c17b19ad945ef693ee423c91dbdd450
raad1masum <piraadwan@gmail.com>
2020-08-08 15:29:38 -0700

    create char letter

--- src/main.c
+++ src/main.c
@@ -6,7 +6,8 @@
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
         for (int j = 0; j < strlen(argv[i]); ++ j) {
-            printf("%c\n", argv[i][j]);
+            char letter = argv[i][j];
+            printf("%c\n", letter);
         }
     }
 
