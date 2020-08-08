6349f058df5132b923f5165c19c4bdc1842001c6
raad1masum <piraadwan@gmail.com>
2020-08-08 15:25:01 -0700

    get letter in each string

--- src/main.c
+++ src/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_STRING_SIZE 999999999999999
 
 int main(int argc, char *argv[]) {
-    for (int i=1; i<argc; ++i) {
-        printf("%s\n", argv[i]);
+    for (int i = 1; i < argc; ++i) {
+        for (int j = 0; j < strlen(argv[i]); ++ j) {
+            printf("%c\n", argv[i][j]);
+        }
     }
 
     return 0;
