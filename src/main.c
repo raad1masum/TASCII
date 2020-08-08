1f16c9c7e9b0e52d96703a0e73d3de63dbed8741
raad1masum <piraadwan@gmail.com>
2020-08-08 15:40:35 -0700

    create tascii_letter function

--- src/main.c
+++ src/main.c
@@ -3,11 +3,16 @@
 
 #define MAX_STRING_SIZE 999999999999999
 
+char tascii_letter(char letter) {
+    printf("%c\n", letter);
+}
+
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
         for (int j = 0; j < strlen(argv[i]); ++ j) {
             char letter = argv[i][j];
-            printf("%c\n", letter);
+
+            tascii_letter(letter);
         }
     }
 
