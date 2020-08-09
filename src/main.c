5b19f6c214899e89a6999a876a1fd996e806cdbe
raad1masum <piraadwan@gmail.com>
2020-08-08 20:58:19 -0700

    clean up code a bit

--- src/main.c
+++ src/main.c
@@ -5,9 +5,8 @@ void tascii_letter(char letter);
 
 int main(int argc, char *argv[]) {
     for (int i = 1; i < argc; ++i) {
-        for (int j = 0; j < strlen(argv[i]); ++ j) {
+        for (int j = 0; j < strlen(argv[i]); ++j) {
             char letter = argv[i][j];
-
             tascii_letter(letter);
         }
     }
